f5c124ae3b35778aa730a40c7bfe76573ed5c4a9
Abel Mettzner <mettznermello@gmail.com>
2026-05-11 09:20:45 -0300

    é isso oi oi oi

--- Script/ex1.js
+++ Script/ex1.js
@@ -1,11 +1,13 @@
 function ex1() {
     const idade = Number(prompt("Digite sua idade:"));
-    if (idade >= 18) {
-        alert("você pode votar");
+    if (idade >= 18 && idade < 65) {
+        alert("você pode votar")
     } else if (idade >= 16 && idade <= 17) {
-        alert("você pode votar, mas precisa de autorização");
+        alert("você pode votar, mas não precisa")
+    } else if (idade >= 65) {
+        alert("você pode votar, apenas se quiser")
     } else {
-        alert("você não pode votar");
+        alert("você não pode votar")
     }
 }
 const buttonex1 = document.getElementById("ex1")
